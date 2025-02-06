f8d6531019be8a6588a103a5883c2f46f8bee4d8
Prasetyo Fajar Islam <126051293+kingpoi06@users.noreply.github.com>
2025-02-07 01:46:03 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ axios.defaults.withCredentials = true;
 // axios.defaults.baseURL = "http://36.88.12.26:5000";
 // axios.defaults.baseURL = "https://akusehat-api-rivopncpgq-et.a.run.app";
 // axios.defaults.baseURL = "https://api-geoheritage.onrender.com";
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://api-jurnalpolri.onrender.com";
 // axios.defaults.baseURL = "https://be-isena-fktp.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
